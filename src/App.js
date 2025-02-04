c0282526252cc062f5b1ccb578b5a4b6eaaefe70
Cubansky <jurko551@gmail.com>
2025-02-05 00:05:11 +0100

    add-movie

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import OneMovie from './pages//movies/OneMovie'
 import AllSerials from './pages//serials/AllSerials'
 import OneSerial from './pages/serials/OneSerial'
 import SharedLayout from './pages/layout/SharedLayout'
+import Form from './pages/form/Form'
 
 const App = () => {
     return <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
@@ -16,6 +17,7 @@ const App = () => {
               <Route path='/' element={ <SharedLayout /> } >
                 <Route index element={ <Home /> } />
                 <Route path='/all-movies' element={ <AllMovies /> } />
+                <Route path='form' element={ <Form /> } />
                 <Route path='/all-serials' element={ <AllSerials /> } /> 
                 <Route path='/one-movie/:movieId' element={ <OneMovie /> } />
                 <Route path='/one-serial/:serialId' element={ <OneSerial /> } />
---
+++ src/pages/form/Form.js
@@ -0,0 +1,200 @@
+import "./Form.css";
+import { useState } from "react";
+import { projectFirestore } from "../../firebase/config";
+
+const Form = () => {
+  const [Title, setTitle] = useState("");
+  const [Age, setAge] = useState(null);
+  const [Time, setTime] = useState(null);
+  const [Description, setDescription] = useState("");
+  const [YoutubeId, setYoutubeId] = useState("");
+  const [Evaluatedi, setEvaluatedi] = useState(null);
+  const [Actors, setActors] = useState([]);
+  const [Director, setDirector] = useState("");
+  const [Scenario, setScenario] = useState("");
+  const [SmallImgUrl, setSmallImgUrl] = useState("");
+  const [LargeImgUrl, setLargeImgUrl] = useState("");
+  const [Country, setCountry] = useState([]);
+  const [Genres, setGenres] = useState([]);
+  const [Year, setYear] = useState(null);
+
+  const handleActorsChange = (e) => {
+    setActors(e.target.value.split(",")); // Předpokládáme, že herci budou zadáváni jako čárkami oddělený seznam
+  };
+
+  const handleGenresChange = (e) => {
+    setGenres(e.target.value.split(",")); // Předpokládáme, že žánry budou zadávány jako čárkami oddělený seznam
+  };
+
+  const handleCountryChange = (e) => {
+    setCountry(e.target.value.split(",")); // Předpokládáme, že žánry budou zadávány jako čárkami oddělený seznam
+  };
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+
+    const newMovie = {
+      title: Title,
+      minage: parseInt(Age),
+      time: parseInt(Time),
+      description: Description,
+      youtube_id: YoutubeId,
+      evaluatedi: parseInt(Evaluatedi),
+      director: Director,
+      scenario: Scenario,
+      small_img_url: SmallImgUrl,
+      large_img_url: LargeImgUrl,
+      actors: Actors,
+      genres: Genres,
+      country: Country,
+      year: Year,
+    };
+
+    try {
+      await projectFirestore.collection("movies").add(newMovie);
+      setTitle("");
+      setAge(null);
+      setTime(null);
+      setDescription("");
+      setYoutubeId("");
+      setEvaluatedi(null);
+      setActors([]);
+      setDirector("");
+      setScenario("");
+      setSmallImgUrl("");
+      setLargeImgUrl("");
+      setCountry([]);
+      setGenres([]);
+      setYear(null);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  return (
+    <section className="form-section">
+      <h1>Přidání filmů</h1>
+      <form onSubmit={submitForm}>
+        <input
+          className="input"
+          type="text"
+          placeholder="Název filmu"
+          onChange={(e) => setTitle(e.target.value)}
+          value={Title}
+        />
+
+        <input
+          className="input"
+          type="number"
+          placeholder="Minimální věk"
+          min="1"
+          onChange={(e) => setAge(e.target.value)}
+          value={Age}
+        />
+
+        <input
+          className="input"
+          type="number"
+          placeholder="Čas filmu"
+          min="1"
+          onChange={(e) => setTime(e.target.value)}
+          value={Time}
+        />
+
+        <input
+          className="input"
+          type="text"
+          placeholder="Obsah"
+          onChange={(e) => setDescription(e.target.value)}
+          value={Description}
+        />
+
+        <input
+          className="input"
+          type="text"
+          placeholder="youtube ID trailer"
+          onChange={(e) => setYoutubeId(e.target.value)}
+          value={YoutubeId}
+        />
+
+        <input
+          className="input"
+          type="number"
+          placeholder="Hodnocení"
+          min="1"
+          onChange={(e) => setEvaluatedi(e.target.value)}
+          value={Evaluatedi}
+        />
+
+        <input
+          className="input"
+          type="text"
+          placeholder="Odkaz na malý obrázek"
+          onChange={(e) => setSmallImgUrl(e.target.value)}
+          value={SmallImgUrl}
+        />
+
+        <input
+          className="input"
+          type="text"
+          placeholder="Odkaz na velký obrázek"
+          onChange={(e) => setLargeImgUrl(e.target.value)}
+          value={LargeImgUrl}
+        />
+
+        <input
+          className="input"
+          type="number"
+          placeholder="Rok vydání"
+          min="1"
+          onChange={(e) => setYear(e.target.value)}
+          value={Year}
+        />
+
+        <input
+          className="input"
+          type="text"
+          onChange={handleActorsChange}
+          value={Actors.join(',')}
+          placeholder="Herci oddelené čárkou !"
+        />
+
+        <input
+          className="input"
+          type="text"
+          placeholder="Země oddelentexté čárkou !"
+          onChange={handleCountryChange}
+          value={Country.join(',')}
+        />
+
+        <input
+          className="input"
+          type="text"
+          placeholder="Žánre oddelené čárkou !"
+          onChange={handleGenresChange}
+          value={Genres.join(',')}
+        />
+
+        <input
+          className="input"
+          type="text"
+          placeholder="Režisér"
+          onChange={(e) => setDirector(e.target.value)}
+          value={Director}
+        />
+
+        <input
+          className="input"
+          type="text"
+          placeholder="Scénář"
+          onChange={(e) => setScenario(e.target.value)}
+          value={Scenario}
+        />
+
+        <input type="submit" value="Přidat film" />
+      </form>
+    </section>
+  );
+};
+
+export default Form;
