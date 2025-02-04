3e4ee6dda1be14988fef1c63fcc6aa405f0a1bc0
Cubansky <jurko551@gmail.com>
2025-02-04 05:36:52 +0100

    redirects

--- src/pages/Trailer.js
+++ src/pages/Trailer.js
@@ -1,10 +1,11 @@
 import "./Trailer.css";
 
 const Trailer = ({ videoId }) => {
-  return <div>
+  return <div className="trailer">
       <iframe
               className="video"
               src={`https://www.youtube.com/embed/${videoId}`}
+              style={{ aspectRatio: "16 / 9" }}
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
