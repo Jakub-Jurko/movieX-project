ccb6401276432018a8e095ba47f0eaf0119e1be8
Cubansky <jurko551@gmail.com>
2025-02-09 03:50:49 +0100

    register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
-import { projectAuth } from "../firebase/config";
+import { projectAuth, googleProvider } from "../firebase/config";
 import { useNavigate } from "react-router-dom";
+import "./Register.css";
+import backgroundImage from "../images/background-moviex.jpg";
+import { BsGoogle } from "react-icons/bs";
 
 const Register = () => {
   const [email, setEmail] = useState("");
@@ -18,15 +21,48 @@ const Register = () => {
     }
   };
 
+  const handleGoogleRegister = async () => {
+    try {
+      const res = await projectAuth.signInWithPopup(googleProvider);
+      navigate("/");
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
   return (
-    <div>
-      <h2>Registrace</h2>
-      {error && <p>{error}</p>}
-      <form onSubmit={handleRegister}>
-        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-        <input type="password" placeholder="Heslo" onChange={(e) => setPassword(e.target.value)} />
-        <button type="submit">Registrovat se</button>
-      </form>
+    <div className="register-page">
+      <div
+        className="background-register"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+        }}
+      ></div>
+      <div className="register-form">
+        <h2>Registrace</h2>
+        {error && <p>{error}</p>}
+        <form className="email-register" onSubmit={handleRegister}>
+          <input
+            className="input-register"
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="input-register"
+            type="password"
+            placeholder="Heslo"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Registrovat se</button>
+        </form>
+        <p class="divider-text">nebo</p>
+
+        <button className="google-register" onClick={handleGoogleRegister}>
+          <BsGoogle />
+          Registrovat se přes Google
+        </button>
+      </div>
     </div>
   );
 };
